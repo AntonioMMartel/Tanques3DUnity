beb3ae2acc831e18d8687e89f3fe8393fdfe3a51
Antonio ML <AntonioMMartel@gmail.com>
2026-02-18 17:53:32 +0000

    Fixed player fire rate issues

--- Assets/Scripts/TankAttack.cs
+++ Assets/Scripts/TankAttack.cs
@@ -7,8 +7,34 @@ public class TankAttack : MonoBehaviour
 {
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] Transform firePoint;
+    [SerializeField] float fireRate = 2f; 
+    private bool isFiring = false;
+    private float fireTimer = 0f;
+
+    private void Update()
+    {
+        if (isFiring)
+        {
+            fireTimer += Time.deltaTime;
+            if (fireTimer >= 1f / fireRate)
+            {
+                Shoot();
+                fireTimer = 0f;
+            }
+        }
+    }
     public void Fire(InputAction.CallbackContext context)
+    {
+        if (context.started)   // Mouse button pressed
+            isFiring = true;
+
+        if (context.canceled)  // Mouse button released
+            isFiring = false;
+    }
+    
+    private void Shoot()
     {
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
+
 }
